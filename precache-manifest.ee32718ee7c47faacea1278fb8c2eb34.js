1f8f1868c4224e4d25cdf1640148029a5d479c73
mac0607 <mayank.buade@gmail.com>
2020-10-12 23:16:22 +0530

    Updates

---
+++ precache-manifest.ee32718ee7c47faacea1278fb8c2eb34.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "be9590b6eed875b6ae9a",
+    "url": "/test-6/static/css/main.9937ca11.chunk.css"
+  },
+  {
+    "revision": "be9590b6eed875b6ae9a",
+    "url": "/test-6/static/js/main.be9590b6.chunk.js"
+  },
+  {
+    "revision": "f1a2edbf6ef623f38f0d",
+    "url": "/test-6/static/js/runtime~main.f1a2edbf.js"
+  },
+  {
+    "revision": "b61b69e70dfd63afa378",
+    "url": "/test-6/static/css/2.0cc20374.chunk.css"
+  },
+  {
+    "revision": "b61b69e70dfd63afa378",
+    "url": "/test-6/static/js/2.b61b69e7.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/test-6/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/test-6/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/test-6/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/test-6/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/test-6/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "3fee8bdcf78af69b15e35acd46d1a1da",
+    "url": "/test-6/index.html"
+  }
+];
